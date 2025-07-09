a3817daed93b9beda8f5ca99e77e28dcd8f9a973
M4RC0Sx <marcos3as3@gmail.com>
2025-07-09 20:56:11 +0200

    feat(welcome): handle welcoma settings model stuff

--- progandbot/db/models/guild_config.py
+++ progandbot/db/models/guild_config.py
@@ -6,6 +6,12 @@ from sqlmodel import Field
 from sqlmodel import SQLModel
 
 
+DEFAULT_WELCOME_MESSAGE = (
+    "Welcome %MEMBER% to the server! Enjoy your stay!"
+    " If you have any questions, feel free to ask."
+)
+
+
 class GuildConfig(SQLModel, table=True):
     __tablename__ = "guild_configs"
 
@@ -14,4 +20,13 @@ class GuildConfig(SQLModel, table=True):
         sa_column=Column(BigInteger, primary_key=True, autoincrement=False),
     )
 
-    welcome_channel_id: int | None = Field(default=None, index=True)
+    welcome_enabled: bool = Field(
+        default=False, sa_column_kwargs={"server_default": "false"}
+    )
+    welcome_channel_id: int | None = Field(
+        default=None, sa_column=Column(BigInteger, index=True)
+    )
+    welcome_message: str = Field(
+        default=DEFAULT_WELCOME_MESSAGE,
+        sa_column_kwargs={"server_default": DEFAULT_WELCOME_MESSAGE},
+    )
